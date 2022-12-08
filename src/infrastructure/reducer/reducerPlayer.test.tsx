8f619bf7f6db95442f40d4e691936a1b30d70bb2
marcos <marcos.bonet@live.com.ar>
2022-12-08 20:17:10 +0100

    custom hooks

--- src/infrastructure/reducer/reducerPlayer.test.tsx
+++ src/infrastructure/reducer/reducerPlayer.test.tsx
@@ -1,57 +1,70 @@
-export {};
-// import { PlayerTypes } from '../models/player.types';
-// import { actionPlayerTypes } from './actionTypesPlayer';
-// import { PlayerReducer } from './reducerPlayer';
+import { PlayerTypes, PlayerWithToken } from '../models/player.types';
+import { actionPlayerTypes } from './actionTypesPlayer';
+import { PlayerReducer } from './reducerPlayer';
 
-// describe('Given the function PlayerReducer', () => {
-//     const PlayerMock: PlayerTypes = {
-//         id: '',
-//         playerName: '',
-//         password: '',
-//         email: '',
-//         matches: [],
-//     };
+describe('Given the function PlayerReducer', () => {
+    const PlayerMock: PlayerTypes = {
+        id: '123',
+        playerName: '',
+        password: '',
+        email: '',
+        matches: [],
+    };
 
-//     let action: { type: string; payload: unknown };
-//     let state: {
-//         isLogged: false;
-//         playerName: 'string';
-//         password: 'string';
-//         token: 'string';
-//     };
+    let action: { type: string; payload: PlayerWithToken };
+    let state: {
+        isLogged: boolean;
+        player: PlayerTypes;
+        token: string;
+    };
 
-//     describe('When the action is login', () => {
-//         beforeEach(() => {
-//             action = {
-//                 type: actionPlayerTypes.login,
-//                 payload: [PlayerMock],
-//             };
-//         //     state = {
+    describe('When the action is login', () => {
+        beforeEach(() => {
+            action = {
+                type: actionPlayerTypes.login,
+                payload: { player: PlayerMock, token: 'token' },
+            };
+            state = {
+                isLogged: false,
+                player: {
+                    email: '',
+                    id: '',
+                    matches: [],
+                    password: '',
+                    playerName: '',
+                },
+                token: '',
+            };
+        });
+        test('Then the returned state should be the action payload', () => {
+            const result = PlayerReducer(state, action);
+            console.log(result.player);
+            expect(result).toEqual({ ...action.payload, isLogged: true });
+        });
+    });
 
-//         //     };
-//         // });
-//         // test('Then the returned state should be the action payload', () => {
-//         //     const result = PlayerReducer(state, action);
-//         //     expect(result).toEqual(action.payload);
-//         // });
-//     });
+    describe('When the action is logout', () => {
+        beforeEach(() => {
+            action = {
+                type: actionPlayerTypes.logout,
+                payload: { player: PlayerMock, token: 'token' },
+            };
+            state = {
+                isLogged: true,
+                player: {
+                    email: '',
+                    id: '',
+                    matches: [],
+                    password: '',
+                    playerName: '',
+                },
 
-//     describe('When the action is Create', () => {
-//         beforeEach(() => {
-//             action = {
-//                 type: actionPlayerTypes.logout,
-//                 payload: [PlayerMock],
-//             };
-//             state = {
-//                 isLogged: false,
-//                 playerName: 'string',
-//                 password: 'string',
-//                 token: 'string',
-//             };
-//         });
-//         test('Then the returned state should include the action payload', () => {
-//             const result = PlayerReducer(state, action);
-//             expect(result).toEqual(action.payload);
-//         });
-//     });
-// });
+                token: null,
+            };
+        });
+        test('Then the returned state should include the action payload', () => {
+            const result = PlayerReducer(state, action);
+            expect(result).toEqual(action.payload);
+        });
+    });
+});
