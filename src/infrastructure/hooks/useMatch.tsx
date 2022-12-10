a5685c867943b9a4c9529b75bd7f833c2a0c524a
marcos <marcos.bonet@live.com.ar>
2022-12-10 14:42:41 +0100

    update app

--- src/infrastructure/hooks/useMatch.tsx
+++ src/infrastructure/hooks/useMatch.tsx
@@ -4,12 +4,14 @@ import { MatchRepo } from '../services/matchRepo';
 import { rootState } from '../store/store';
 import * as ac from '../reducer/actionCreatorMatch';
 import { ProtoMatch } from '../models/match.types';
+import { PlayerRepo } from '../services/playerRepo';
 
 export const useMatch = () => {
     const matches = useSelector((state: rootState) => state.matches);
-    console.log(matches, '2');
+
     const dispatcher = useDispatch();
     const apiMatch = useMemo(() => new MatchRepo(), []);
+    const apiPlayer = useMemo(() => new PlayerRepo(), []);
     useEffect(() => {
         apiMatch
             .get()
@@ -23,9 +25,26 @@ export const useMatch = () => {
             .then((matches) => dispatcher(ac.createActionCreator(matches)))
             .catch((error: Error) => console.log(error.name, error.message));
     };
+    const handleUpdateAddMatch = async (idMatch: string) => {
+        await apiPlayer
+            .updateadd(idMatch)
+            .then((matchUpdated) =>
+                dispatcher(ac.updateAddActionCreator(matchUpdated))
+            )
+            .catch((error: Error) => console.log(error.name, error.message));
+    };
+
+    // const handleUpdateDeleteMatch = async (id: string) => {
+    //     await apiPlayer
+    //         .updatedelete(id)
+    //         .then(() => dispatcher(ac.updateDeleteActionCreator()))
+    //         .catch((error: Error) => console.log(error.name, error.message));
+    // };
 
     return {
         matches,
         handleCreateMatch,
+        handleUpdateAddMatch,
+        //handleUpdateDeleteMatch,
     };
 };
